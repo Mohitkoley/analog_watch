0f423c84275b94414482174e1b0d893270e450fb
Mohit <mohitkoley5@gmail.com>
2024-01-29 17:27:30 +0530

    added path

--- lib/customPaints/analog_watch_painter.dart
+++ lib/customPaints/analog_watch_painter.dart
@@ -10,7 +10,22 @@ class AnalogWatchPainter extends CustomPainter {
       ..style = PaintingStyle.fill;
 
     canvas.drawCircle(
-        Offset(size.width / 2, size.height / 2), size.width / 2, paint);
+        Offset(size.width / 2, size.height / 2), size.width / 6, paint);
+    final path = Path();
+    path.moveTo(100, 100);
+    path.lineTo(200, 50);
+    path.quadraticBezierTo(300, 150, 250, 250);
+    path.arcToPoint(Offset(150, 200),
+        radius: Radius.circular(40), clockwise: false);
+    path.cubicTo(100, 200, 50, 150, 100, 100);
+    path.close();
+
+    final pathPaint = Paint()
+      ..color = Colors.blue
+      ..strokeWidth = 5
+      ..style = PaintingStyle.stroke;
+
+    canvas.drawPath(path, pathPaint);
   }
 
   @override
